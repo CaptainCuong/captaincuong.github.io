427c17ac101295aff4c6d392257c5089e64e2353
CaptainCuong <captaincuong1692001@gmail.com>
2024-05-29 22:21:03 +0700

    update clustrmaps

--- _site/find_keyword.py
+++ _site/find_keyword.py
@@ -1,7 +1,8 @@
 import os
 
+file_dir = ['./_site']
 file_dir = ['./_data', './_includes', './_layouts', './_projects', './_website_2014', './assets', './libs']
-keyword = """.*.fas.*"""
+keyword = """.*Vitae.*"""
 
 for dir_ in file_dir:
 	print('\n','-'*25,dir_,'-'*25,'\n')
--- find_keyword.py
+++ find_keyword.py
@@ -1,7 +1,8 @@
 import os
 
+file_dir = ['./_site']
 file_dir = ['./_data', './_includes', './_layouts', './_projects', './_website_2014', './assets', './libs']
-keyword = """.*https://ieeexplore.ieee.org/document/10225872.*"""
+keyword = """.*Vitae.*"""
 
 for dir_ in file_dir:
 	print('\n','-'*25,dir_,'-'*25,'\n')
